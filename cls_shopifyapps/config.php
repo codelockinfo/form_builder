38ef3c2fb7b037c219cc9e1649cb843903ff3bad
codelockinfo <codelockinfo@gmail.com>
2023-06-28 17:59:28 +0530

    Change secret key and API key

--- cls_shopifyapps/config.php
+++ cls_shopifyapps/config.php
@@ -1,6 +1,6 @@
 <?php
-define("CLS_SHOPIFY_API_KEY", "9875728c6b382b21f2b77786a7b11d06");
-define("SHOPIFY_SECRET", "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");
+define("CLS_SHOPIFY_API_KEY", "de776739597a8c3590a04a65195d6f44");
+define("SHOPIFY_SECRET", "39b3f75287bb3c885c164b04612f1512");
 define("SHOPIFY_SCOPE", "read_content,write_content,read_products,write_products,read_script_tags,write_script_tags");
 define("SITE_PATH","https://codelocksolutions.com/form_builder");
 
