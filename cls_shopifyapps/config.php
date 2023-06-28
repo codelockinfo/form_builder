7a476c807e5927c1860cd4e5ff429389642c6335
codelockinfo <codelockinfo@gmail.com>
2023-06-28 17:50:13 +0530

    Change redirect url

--- cls_shopifyapps/config.php
+++ cls_shopifyapps/config.php
@@ -2,6 +2,6 @@
 define("CLS_SHOPIFY_API_KEY", "9875728c6b382b21f2b77786a7b11d06");
 define("SHOPIFY_SECRET", "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");
 define("SHOPIFY_SCOPE", "read_content,write_content,read_products,write_products,read_script_tags,write_script_tags");
-define("SITE_PATH","https://codelocksolutions.com/form_builder/");
+define("SITE_PATH","https://codelocksolutions.com/form_builder");
 
 ?>
