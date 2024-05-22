5380549b35bd527c5faaa9162d653d0ea13ea5b2
codelockinfo <codelockinfo@gmail.com>
2024-05-22 09:52:59 +0530

    fetch all form table data index page

--- user/create-new-form.php
+++ user/create-new-form.php
@@ -539,85 +539,83 @@ include_once('cls_header.php');
             </div>
         </form>
     </div>
-    <!-- <script>
-    const ctx = document.getElementById('chart').getContext('2d');
-         const chart = new Chart(ctx, {
-             // The type of chart we want to create
-             type: 'bar',
-              data: {
-               labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-               datasets: [{
-                   label: '# of Votes',
-                   data: [12, 19, 3, 5, 2, 3],
-                   backgroundColor: [
-                       'rgba(255, 99, 132, 0.2)',
-                       'rgba(54, 162, 235, 0.2)',
-                       'rgba(255, 206, 86, 0.2)',
-                       'rgba(75, 192, 192, 0.2)',
-                       'rgba(153, 102, 255, 0.2)',
-                       'rgba(255, 159, 64, 0.2)'
-                   ],
-                   borderColor: [
-                       'rgba(255,99,132,1)',
-                       'rgba(54, 162, 235, 1)',
-                       'rgba(255, 206, 86, 1)',
-                       'rgba(75, 192, 192, 1)',
-                       'rgba(153, 102, 255, 1)',
-                       'rgba(255, 159, 64, 1)'
-                   ],
-                   borderWidth: 1
-               }]
-           },
-             // Configuration options go here
-             options: {
-               responsive: true,
-               scales: {
-                 xAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}],
-                 // yAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}]
+    <script>
+        //  const ctx = document.getElementById('chart').getContext('2d');
+        //  const chart = new Chart(ctx, {
+        //      type: 'bar',
+        //       data: {
+        //        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        //        datasets: [{
+        //            label: '# of Votes',
+        //            data: [12, 19, 3, 5, 2, 3],
+        //            backgroundColor: [
+        //                'rgba(255, 99, 132, 0.2)',
+        //                'rgba(54, 162, 235, 0.2)',
+        //                'rgba(255, 206, 86, 0.2)',
+        //                'rgba(75, 192, 192, 0.2)',
+        //                'rgba(153, 102, 255, 0.2)',
+        //                'rgba(255, 159, 64, 0.2)'
+        //            ],
+        //            borderColor: [
+        //                'rgba(255,99,132,1)',
+        //                'rgba(54, 162, 235, 1)',
+        //                'rgba(255, 206, 86, 1)',
+        //                'rgba(75, 192, 192, 1)',
+        //                'rgba(153, 102, 255, 1)',
+        //                'rgba(255, 159, 64, 1)'
+        //            ],
+        //            borderWidth: 1
+        //        }]
+        //    },
+        //      // Configuration options go here
+        //      options: {
+        //        responsive: true,
+        //        scales: {
+        //          xAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}],
+        //          // yAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}]
                  
-               }
-             }
-         });
+        //        }
+        //      }
+        //  });
         
-         const ctx2 = document.getElementById('chart2').getContext('2d');
-         const chart2 = new Chart(ctx2, {
-             // The type of chart we want to create
-             type: 'bar',
-              data: {
-               labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-               datasets: [{
-                   label: '# of Votes',
-                   data: [12, 19, 3, 5, 2, 3],
-                   backgroundColor: [
-                       'rgba(255, 99, 132, 0.2)',
-                       'rgba(54, 162, 235, 0.2)',
-                       'rgba(255, 206, 86, 0.2)',
-                       'rgba(75, 192, 192, 0.2)',
-                       'rgba(153, 102, 255, 0.2)',
-                       'rgba(255, 159, 64, 0.2)'
-                   ],
-                   borderColor: [
-                       'rgba(255,99,132,1)',
-                       'rgba(54, 162, 235, 1)',
-                       'rgba(255, 206, 86, 1)',
-                       'rgba(75, 192, 192, 1)',
-                       'rgba(153, 102, 255, 1)',
-                       'rgba(255, 159, 64, 1)'
-                   ],
-                   borderWidth: 1
-               }]
-           },
-             // Configuration options go here
-             options: {
-               responsive: true,
-               scales: {
-                 xAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}],
-                 // yAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}]
+        //  const ctx2 = document.getElementById('chart2').getContext('2d');
+        //  const chart2 = new Chart(ctx2, {
+        //      type: 'bar',
+        //       data: {
+        //        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        //        datasets: [{
+        //            label: '# of Votes',
+        //            data: [12, 19, 3, 5, 2, 3],
+        //            backgroundColor: [
+        //                'rgba(255, 99, 132, 0.2)',
+        //                'rgba(54, 162, 235, 0.2)',
+        //                'rgba(255, 206, 86, 0.2)',
+        //                'rgba(75, 192, 192, 0.2)',
+        //                'rgba(153, 102, 255, 0.2)',
+        //                'rgba(255, 159, 64, 0.2)'
+        //            ],
+        //            borderColor: [
+        //                'rgba(255,99,132,1)',
+        //                'rgba(54, 162, 235, 1)',
+        //                'rgba(255, 206, 86, 1)',
+        //                'rgba(75, 192, 192, 1)',
+        //                'rgba(153, 102, 255, 1)',
+        //                'rgba(255, 159, 64, 1)'
+        //            ],
+        //            borderWidth: 1
+        //        }]
+        //    },
+        //      // Configuration options go here
+        //      options: {
+        //        responsive: true,
+        //        scales: {
+        //          xAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}],
+        //          // yAxes: [{ticks: {beginAtZero: true}, gridLines: {zeroLineColor: 'red', drawBorder: true, display: true, zeroLineWidth: 5}}]
                  
-               }
-             }
-         });
-         $(document).ready(function() {
+        //        }
+        //      }
+        //  });
+        $(document).ready(function() {
             getAllForm();
-    });
-    </script> -->
+        });
+    </script>
