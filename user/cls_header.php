a37cfa040096dd4d226226b12d8b321b186e02b9
codelock <codelockinfo@gmail.com>
2026-01-01 15:44:32 +0530

    Complete publish POP-UP

--- user/cls_header.php
+++ user/cls_header.php
@@ -41,7 +41,7 @@ $view = (isset($_GET["view"]) && $_GET["view"]) ? $_GET["view"] : FALSE;
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
         <script src="<?php echo main_url('assets/js/owl.carousel.js'); ?>"></script>
         <script src="<?php echo main_url('assets/js/owl.carousel.min.js'); ?>"></script>
-        <script src="<?php echo main_url('assets/js/style.js'); ?>"></script>
+        <script src="<?php echo main_url('assets/js/style1.js'); ?>"></script>
         <script src="<?php echo main_url('assets/js/shopify_client.js'); ?>"></script>
   
    
