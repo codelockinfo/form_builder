87b49d67ed737e2414fd6751896cbb7f4bf26fa5
codelock <codelockinfo@gmail.com>
2026-01-08 11:50:16 +0530

    Data preview design

--- user/dashboard_submissions.php
+++ user/dashboard_submissions.php
@@ -583,6 +583,20 @@ if (isset($_GET['shop']) && $_GET['shop'] != '') {
 
 <?php include_once('dashboard_footer.php'); ?>
 
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/677e4e7faf5bfec1dbe85825/1ih2m597m';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
 <!-- Chart.js Library -->
 <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
 
